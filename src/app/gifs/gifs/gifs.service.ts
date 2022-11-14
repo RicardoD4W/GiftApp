1c794e8b6b644d47c759ec249816091e0c926323
ricardod4w <ricardo.rodriguez@fernando3martos.com>
2022-11-14 14:34:08 +0100

    add

--- src/app/gifs/gifs/gifs.service.ts
+++ src/app/gifs/gifs/gifs.service.ts
@@ -6,4 +6,32 @@ import { Injectable } from '@angular/core';
 export class GifsService {
 
   constructor() { }
+
+  private API_KEY : string = 'hXOKPK2TYHBfgjrwL60pCjwjgo0G0f45';
+
+
+  busquedasRealizadas : string[] = [];
+
+  getBusquedas() : string[]{
+    return this.busquedasRealizadas;
+  }
+
+  addBusqueda(busqueda : string) : void {
+    let busq = busqueda.toLocaleUpperCase();
+
+    if(busqueda !== ''){
+      if(this.getBusquedas().length > 10){
+          this.getBusquedas().pop();
+          this.busquedasRealizadas.unshift(busq);
+        }else{
+          if(this.getBusquedas().includes(busq)){
+            console.log('Repetida');
+          }else{        
+          this.busquedasRealizadas.unshift(busq);
+          }
+        }  
+          }else{
+            alert('No se permite hacer búsquedas vacías');
+          }
+  }
 }
--- src/app/gifs/gifs/search/search.component.ts
+++ src/app/gifs/gifs/search/search.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { GifsService } from '../gifs.service';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+@ViewChild('texto') texto! : ElementRef<HTMLInputElement>;
 
-  constructor() { }
+  constructor(private gifsService : GifsService) { }
 
   ngOnInit(): void {
   }
 
+
+  busqueda(texto : string){
+      this.gifsService.addBusqueda(texto);
+      this.texto.nativeElement.value = '';
+  }
 }
